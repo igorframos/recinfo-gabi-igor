07df56b3b5966ee3540199bbaf874e1fcdd70a4f
igor.bread <igor.bread@1a17cf69-1cd1-d244-a54b-466ecdfec55d>
2011-10-07 01:58:24 +0000

    Já funciona mais ou menos, acho. Falta aumentar o limite de tweets que ele pode buscar (estou buscando 100 por query, então nem precisa ser mais que isso) e ver o lance da ordenação.

---
+++ trunk/src/transitorj/model/Crawler.java
@@ -0,0 +1,65 @@
+package transitorj.model;
+
+import java.io.IOException;
+import java.util.List;
+
+import twitter4j.Tweet;
+
+public class Crawler extends Thread {
+
+	private static final int SLEEP_TIME = 60000;
+	private BuscaTweets buscaTweets = new BuscaTweets();
+	private IndexadorDeTweets indexador = null;
+	private List<Tweet> mensagens = null;
+	private int indexados;
+	Tweet last;
+
+	public Crawler() {
+		last = null;
+		
+		try {
+			/*
+			 * indexador = new IndexadorDeTweets(
+			 * "C:/Users/Gabriel/Desktop/Gabriel/Faculdade/RecInfo/Workspace/recinfo-gabi-igor"
+			 * ); System.out.println("Criado índice.");
+			 */
+
+			indexador = new IndexadorDeTweets(
+					"/home/igorfr/workspace/twitterindex");
+			System.out.println("Criado índice.");
+
+		} catch (IOException e) {
+			System.out.println("Erro ao criar índice.");
+		}
+	}
+
+	@Override
+	public void run() {
+		try {
+			while (true) {
+				mensagens = buscaTweets.buscaTodos(last);
+				System.out.println("Tweets buscados.");
+				
+				if (mensagens.size() > 0)
+					last = mensagens.get(mensagens.size()-1);
+				
+				for (Tweet t : mensagens) {
+					System.out.println(t.getCreatedAt());
+					System.out.println(t.getFromUser() + ": " + t.getText() + "\n");
+				}
+
+				indexados = indexador.indexa(mensagens);
+				System.out.println("Tweets indexados: " + indexados);
+
+				Thread.sleep(SLEEP_TIME);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			System.out
+					.println("Crawler morreu. Tweets não serão mais atualizados.");
+			e.printStackTrace();
+		}
+	}
+
+}
