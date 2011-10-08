1e5531ef403a0a831dac36a5ec18ab73e7f330be
igor.bread <igor.bread@1a17cf69-1cd1-d244-a54b-466ecdfec55d>
2011-10-08 01:02:52 +0000

    Eu fiquei muito confuso ontem com versões e coisa e tal. Acho que fiz o merge certo, mas era bom conferir. Tô commitando uma versão que funciona aqui.

--- src/transitorj/view/TelaBusca.java
+++ src/transitorj/view/TelaBusca.java
@@ -14,93 +14,95 @@ import javax.swing.JList;
 import javax.swing.JTextField;
 
 import transitorj.controller.ControleBusca;
+import transitorj.model.Crawler;
 
 public class TelaBusca extends JFrame implements ActionListener {
-	
-	private static final long serialVersionUID = 1L;
-	
-	JLabel labelRua,labelErro;
-	JTextField textRua;
-	JButton botaoPesquisar;
-	JList<String> listaTweets;
-	ControleBusca controleBusca;
-	
-	public TelaBusca() {
-		setTitle("Tr‚nsito RJ App");
-		
-		controleBusca = new ControleBusca();
-		
-		inicializaComponentes();
-		carregaUI();
-		pack();
-		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-		setSize(800, 600); 
-	}
-	
-	public void inicializaComponentes() {
-		// Labels
-		labelRua = new JLabel("Rua:");
-		labelErro = new JLabel("N„o insira o tipo de logradouro no campo de busca." + '\n' + "Ex: Para buscar Avenida Brasil, digite apenas Brasil");
-		
-		// TextField
-		textRua = new JTextField(30);
-		
-		// Botıes
-		botaoPesquisar = new JButton("Pesquisar");
-		botaoPesquisar.addActionListener(this);
-		
-		// Tweets
-		listaTweets = new JList<String>();
-	}
-	
-	public void carregaUI() {
-		Container conteudo = getContentPane();
-		GridBagLayout layout = new GridBagLayout();
-		GridBagConstraints constraint = new GridBagConstraints();
-		conteudo.setLayout(layout);		
-		
-		constraint.fill = GridBagConstraints.HORIZONTAL;
-		
-		constraint.gridx = 0;
-		constraint.gridy = 0;
-		conteudo.add(labelRua, constraint);
-		
-		constraint.gridx = 1;
-		constraint.gridy = 0;	
-		conteudo.add(textRua, constraint);
-		
-		constraint.gridx = 1;
-		constraint.gridy = 2;
-		conteudo.add(labelErro, constraint);
-		
-		constraint.gridx = 1;
-		constraint.gridy = 3;
-		//constraint.anchor = GridBagConstraints.PAGE_END;		
-		constraint.insets = new Insets(20, 60, 50, 50);
-		conteudo.add(botaoPesquisar, constraint);
-		
-		constraint.gridx = 0;
-		constraint.gridy = 5;
-		constraint.gridwidth = 2;
-		conteudo.add(listaTweets, constraint);
-	}
+        
+		private Crawler crawler = new Crawler();
+        private static final long serialVersionUID = 1L;
+        
+        JLabel labelRua,labelErro;
+        JTextField textRua;
+        JButton botaoPesquisar;
+        JList listaTweets;
+        ControleBusca controleBusca;
+        
+        public TelaBusca() {
+                setTitle("Tr√¢nsito RJ App");
+                
+                controleBusca = new ControleBusca();
+                
+                inicializaComponentes();
+                carregaUI();
+                pack();
+                setVisible(true);
+                setDefaultCloseOperation(EXIT_ON_CLOSE);
+                
+                setSize(800, 600);
+                
+                crawler.run();
+        }
+        
+        public void inicializaComponentes() {
+                // Labels
+                labelRua = new JLabel("Rua:");
+                labelErro = new JLabel("N√£o insira o tipo de logradouro no campo de busca." + '\n' + "Ex: Para buscar Avenida Brasil, digite apenas Brasil");
+                
+                // TextField
+                textRua = new JTextField(30);
+                
+                // Bot√µes
+                botaoPesquisar = new JButton("Pesquisar");
+                botaoPesquisar.addActionListener(this);
+                
+                // Tweets
+                listaTweets = new JList();
+        }
+        
+        public void carregaUI() {
+                Container conteudo = getContentPane();
+                GridBagLayout layout = new GridBagLayout();
+                GridBagConstraints constraint = new GridBagConstraints();
+                conteudo.setLayout(layout);             
+                
+                constraint.fill = GridBagConstraints.HORIZONTAL;
+                
+                constraint.gridx = 0;
+                constraint.gridy = 0;
+                conteudo.add(labelRua, constraint);
+                
+                constraint.gridx = 1;
+                constraint.gridy = 0;   
+                conteudo.add(textRua, constraint);
+                
+                constraint.gridx = 1;
+                constraint.gridy = 2;
+                conteudo.add(labelErro, constraint);
+                
+                constraint.gridx = 1;
+                constraint.gridy = 3;
+                //constraint.anchor = GridBagConstraints.PAGE_END;              
+                constraint.insets = new Insets(20, 60, 50, 50);
+                conteudo.add(botaoPesquisar, constraint);
+                
+                constraint.gridx = 0;
+                constraint.gridy = 5;
+                constraint.gridwidth = 2;
+                conteudo.add(listaTweets, constraint);
+        }
 
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-		if(arg0.getSource() == botaoPesquisar) {			
-			listaTweets.setListData(controleBusca.buscaEntrada(textRua.getText()));			
-		}
-	}
+        @Override
+        public void actionPerformed(ActionEvent arg0) {
+                if(arg0.getSource() == botaoPesquisar) {                        
+                        listaTweets.setListData(controleBusca.buscaEntrada(textRua.getText()));                 
+                }
+        }
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		new TelaBusca();
-	}
+        /**
+         * @param args
+         */
+        public static void main(String[] args) {
+                new TelaBusca();
+        }
 
 }
